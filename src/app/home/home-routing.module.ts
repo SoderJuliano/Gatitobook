3f657284dfd23d45f4f48434a3be4aac4d8df286
Juliano Soder <julianosoder1989@gmail.com>
2021-11-23 12:59:54 -0300

    fourth commit

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,12 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent,
-      }
+      },
+      {
+        path: 'novousuario',
+        component: NovoUsuarioComponent,
+      },
+      
     ]
   }
 ];

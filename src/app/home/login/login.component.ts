89c4e67ac9d7432243d81474d72efd76be23cc85
Juliano Soder <julianosoder1989@gmail.com>
2021-11-23 12:25:36 -0300

    third commit

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   senha=''
 
   constructor(
-    private authService: AutenticacaoService
+    private authService: AutenticacaoService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -22,7 +24,7 @@ export class LoginComponent implements OnInit {
   login(){
     this.authService.autenticar(this.usuario, this.senha).subscribe(
       ()=>{
-        console.log("logado com sucesso")
+        this.router.navigate(['animais'])
       },
       (error) =>{
         console.log(error)
